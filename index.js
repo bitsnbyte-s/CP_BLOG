97ff0d6a3effc4ca183994521f3bfa6c7170c6a2
vivek286 <solankivivek01@gmail.com>
2021-07-06 17:39:30 +0530

    Added solutions

--- index.js
+++ index.js
@@ -34,14 +34,30 @@ $.getJSON('https://spreadsheets.google.com/feeds/cells/1nimQ1WlKMMbDJorNsxPp3gpS
 	// console.log(difficulty);
 	// console.log(sollang);
 	// console.log(solution);
+    var giving_id=i/7;
     output1 += `       
     <div class="card" style="width: 80%; height:20%; margin-top:2%; margin-left:10%;">
   <div class="card-body" style="height:100%; width:100%; overflow:hidden;">
     <h4 class="card-title">${questionname}</h4>
     <h6 class="card-subtitle mb-2 text-muted">${difficulty}</h6>
      <p class="card-text" style="white-space: pre-line;">${description}</p>
-     <button type="button" class="btn btn-primary">View Question</button>
-     <button type="button" class="btn btn-primary">View Solution</button>
+     <div class="d-grid gap-2">
+     <button class="btn btn-primary" type="button" onclick="viewSolution(${giving_id}a)">View Solution</button>
+     
+   </div>
+  </div>
+</div>
+<div class="card text-white bg-success mb-3" style="width: 80%; height:20%; margin-top:2%; margin-left:10%; visibility: block;" id="${giving_id}a">
+  <div class="card-header"><h3>Solution</h3></div>
+  <div class="card-body">
+    <h5 class="card-title">Language:- ${sollang} </h5>
+    <h5 class="card-title">Solution By:- ${name} </h5>
+    <p class="card-text" style="white-space: pre-line;">${solution}</p>
+    <h5 class="card-title">Time:- ${time} </h5>
+    <div class="d-grid gap-2">
+     <button class="btn btn-primary" type="button" onclick="closeSolution(${giving_id}a)" a">Close Solution</button>
+     
+   </div>
   </div>
 </div>
       
@@ -87,3 +103,9 @@ $.getJSON('https://spreadsheets.google.com/feeds/cells/1nimQ1WlKMMbDJorNsxPp3gpS
 });
 // alert(solutions_indx.length);
 console.log(solutions_indx)
+function viewSolution(id){
+    document.getElementById(id).style.visibility = "block";
+}
+function viewSolution(id){
+    document.getElementById(id).style.visibility = "none";
+}
